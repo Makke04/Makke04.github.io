af8e7692d9b69beab679f66601aa5be2dd8f6f03
Makke04 <maxroikojokela@gmail.com>
2021-08-11 12:15:04 +0300

    hello world and loop

---
+++ loops/script.js
@@ -0,0 +1,19 @@
+/*for(let i = 0; i < 3; i++) {
+    for(let i = 0; i < 5; i++) {
+        
+        document.write("#")
+    }
+    document.write("<br>")
+}*/
+
+
+let array =["bmw", "R6s", "valorant"]
+
+
+array.push("LoL")
+
+array.push("Z1")
+
+
+
+
